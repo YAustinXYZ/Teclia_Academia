40f1ac8cafb7e055e46fd3dac2cb21528fd42c7f
Austin Carranza <austinrmz2007@gmail.com>
2026-05-28 19:08:27 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 4173,
-  },
-});
+
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: ['teclia-academia-1.onrender.com']
+  }
+})
