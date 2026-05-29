bc843eada11058c754841e0cb7e31cf54ebec055
Austin Carranza <austinrmz2007@gmail.com>
2026-05-28 21:00:28 -0600

    Update authController.js

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -14,11 +14,12 @@ const createMailTransport = () => {
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT) || 587,
-      secure: process.env.EMAIL_SECURE === 'true',
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        pass: process.env.EMAIL_PASS.replace(/\s/g, ''),
       },
+      requireTLS: true,
     });
   }
 
